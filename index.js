4952a42da9cb0a2ddcbca1c69cd7b9837afafda1
Masoom Nasir <masoomnasir60@gmail.com>
2024-09-22 21:09:21 +0500

    remove debugging statements

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import readLineSync from "readline-sync";
 import colors from "colors";
 import dotenv from "dotenv";
 dotenv.config();
-// Access your API key as an environment variable (see "Set up your API key" above)
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 async function run() {
-  console.log(process.env.GOOGLE_API_KEY);
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
   console.log(colors.bold.green("Welcome to the Chatbot Program! "));
   console.log(colors.bold.green("You can start the chatting with the bot."));
